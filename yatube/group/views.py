4d5c845c4938a06c23ae5b1a8aac048b83fa5900
Никита Лопаткин <n1cklanden@yandex.ru>
2022-01-07 01:35:12 +0300

    Publicating the project

--- yatube/group/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
-def group_posts(request):
-    return HttpResponse('This is the page where you would see groups posts.')
